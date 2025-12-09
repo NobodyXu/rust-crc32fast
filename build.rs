6dcaf392581f342dc64485377bb7c8f54a2823c1
Jiahao XU <30436523+NobodyXu@users.noreply.github.com>
2025-12-10 02:26:45 +1100

    Add cfg always_have_specialized declared in build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,36 @@
 use std::{env, process::Command};
 
+fn declare_cfg(cfg: &str) {
+    println!("cargo:rustc-cfg={}", cfg);
+    println!("cargo:rustc-check-cfg=cfg({})", cfg);
+}
+
 fn main() {
+    let target = std::env::var("TARGET").unwrap();
+    let target_arch = target.split_once("-").unwrap().0;
+    let target_features = std::env::var("CARGO_CFG_TARGET_FEATURE")
+        .unwrap_or_default();
+
+    if
+        (target_arch == "x86" || target_arch == "x86_64")
+        && target_features.contains("sse2")
+        && target_features.contains("pclmulqdq")
+        && target_features.contains("sse4.1")
+    {
+        declare_cfg("always_have_specialized");
+    }
+    
     if let Some(minor_version) = minor_rustc_version() {
         // rustc 1.80 stabilized ARM CRC32 intrinsics:
         // https://doc.rust-lang.org/nightly/core/arch/aarch64/fn.__crc32d.html
         if minor_version >= 80 {
-            println!("cargo:rustc-cfg=stable_arm_crc32_intrinsics");
-            println!("cargo:rustc-check-cfg=cfg(stable_arm_crc32_intrinsics)");
+            declare_cfg("stable_arm_crc32_intrinsics");
+
+            if target_arch == "aarch64"
+                && target_features.contains("crc")
+            {
+                declare_cfg("always_have_specialized");
+            }
         }
     }
 }
